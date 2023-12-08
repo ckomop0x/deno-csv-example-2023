e968a2d3af8aaafd3d9a06a1a3b35fa51320fb72
Pavel Klochkov <ckomop0x@users.noreply.github.com>
2023-12-08 17:49:19 +0100

    Update to return Habitable planets

--- mod.ts
+++ mod.ts
@@ -1,9 +1,12 @@
-import { join } from "https://deno.land/std/path/mod.ts";
+import { join } from "https://deno.land/std@0.208.0/path/mod.ts";
 import { BufReader } from "https://deno.land/std@0.101.0/io/bufio.ts";
 import { parse } from "https://deno.land/std@0.101.0/encoding/csv.ts";
 
+interface Planet {
+    [ key: string ]: string
+}
 
-async function readFile() {
+async function loadPlanetsData() {
     const path = join("data", "NASA exoplanet archive.csv");
     const file = await Deno.open(path);
     const bufReader = new BufReader(file);
@@ -11,10 +14,24 @@ async function readFile() {
         skipFirstRow: true,
         comment: "#",
     });
-    console.log(result);
 
     Deno.close(file.rid);
+
+    const planets = (result as Array<Planet>).filter((planet) => {
+        const planetaryRadius = Number(planet["koi_prad"]);
+        const stellarMass = Number(planet["koi_smass"]);
+        const stellarRadius = Number(planet["koi_srad"]);
+
+        return planet["koi_disposition"] === "CONFIRMED"
+          && planetaryRadius > 0.5 && planetaryRadius < 1.5
+          && stellarMass > 0.78 && stellarMass < 1.04
+          && stellarRadius > 0.99 && stellarRadius < 1.01;
+    });
+
+    return result;
 }
+
+const newEarths = await loadPlanetsData();
 
 // async function readDir() {
 //     for await (const dirEntry of Deno.readDir(Deno.cwd())) {
@@ -22,5 +39,4 @@ async function readFile() {
 //     }
 // }
 
-void readFile();
-// void readDir();
+console.log(`${newEarths.length} habitable planets found!`)
